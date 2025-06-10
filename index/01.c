1118f0b06467fbc62f35d3cc2f3d24672cadb8cd
anvragsingh <anuragsinghania45@gmail.com>
2025-06-10 17:35:56 +0530

    index file upload

---
+++ index/01.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    printf("hello world");
+    return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+
+
+
+
+int main(){
+    
+
+    
+    return 0;
+}
---
+++ index/02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int bc;
+
+    scanf("%d", &bc);
+
+    printf("There are this many bc in the class: %d\n", bc);
+    return 0;
+}
+                                                        
