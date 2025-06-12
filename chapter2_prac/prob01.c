bfd18b0f6210e7e61290991eb134bc3c9667e9bd
anvragsingh <anuragsinghania45@gmail.com>
2025-06-12 16:17:12 +0530

    practice ch.2

---
+++ chapter2_prac/prob01.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    float a = 3.0;
+    int b = 8;
+    int c = 2;
+    printf("value of %f and %d and %dis %f ", a,b,c,a/b-c );
+}
---
+++ chapter2_prac/prob02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a =2;
+    int b = 3;
+    int c = 3;
+    int d = 2;
+    int e = 3*a/b - c+d;
+    printf("value od %d and %d and %d and %d is %d\n", a, b, c, d, 3*a/b - c+d);
+    printf("value of e is %d", e);
+    return 0;
+}
