e3de4c945ca4898fb55d76ef5cf418fba89c8795
anvragsingh <anuragsinghania45@gmail.com>
2025-06-12 16:15:47 +0530

    instr and operators

---
+++ chapter2 (instr and opertors)/arth_optr.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a= 4;
+    int b = 3;
+    int c = a+b;
+    printf("value of a is %d\n and value of b is %d\n and sum is %d", a, b, c);
+    printf("remainder a divided by b : %d", a%b);
+
+
+   
+    return 0;
+}
---
+++ chapter2 (instr and opertors)/variables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int n = 12;
+    float m = 21.22;
+
+    n = (int) m;
+    printf("%d\n", n);
+    return 0;
+}
